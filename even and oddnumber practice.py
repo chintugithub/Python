0575fe0eb8b06a3c7d8e855490df210a62ae99f6
Ashis Rao <ashisrao1999@gmail.com>
2022-02-17 16:31:31 +0530

    Added practice files

---
+++ even and oddnumber practice.py
@@ -0,0 +1,27 @@
+for num in range(1, 101):
+
+    if num % 2 == 0:
+        print(num, end=" ")
+
+for number in range(1, 100):
+
+    if number % 2 == 1:
+        print(number, end=" ")
+
+tup = (12, 24, 34, 54, 33, 53, 57, 79, 91, 97, 55, 48, 22, 10, 90, 56, 46, 60, 70)
+
+for nu_mb in tup:
+
+    if nu_mb % 2 == 0:
+        print(nu_mb, end=" ")
+
+set = {13, 34, 56, 79, 95, 46, 90, 24, 46, 84, 78, 91, 47, 49, 98, 88, 67, 76, 32, 40}
+
+for num_set in set:
+
+    if num_set % 2 == 1:
+        print(num_set, end=" ")
+
+
+
+ 
