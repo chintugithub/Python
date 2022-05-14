a9b3653f861a735d4719c1ab7dd973adfea9284e
Ashis Rao <ashisrao1999@gmail.com>
2022-05-14 20:20:01 +0530

    added Sorted_square list.py

---
+++ Sorted_square list.py
@@ -0,0 +1,21 @@
+# from unittest import result
+
+
+# def sorted_square(array):
+#     result = [x * x for x in array]
+#     result.sort()
+#     return result
+# print(sorted_square([2,5,7,3,-4]))
+
+fun main() {
+  val myNum = 5               // Int
+  val myDoubleNum = 5.99      // Double
+  val myLetter = 'D'          // Char
+  val myBoolean = true        // Boolean
+  val myText = "Hello"        // String
+  println(myNum)
+  println(myDoubleNum)
+  println(myLetter)
+  println(myBoolean)
+  println(myText)
+}
